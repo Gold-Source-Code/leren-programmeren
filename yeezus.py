6f7b4d5a74e8fa733bcad8851dafebafb37a52fb
GlobalOffender <99067159@mydavinci.nl>
2022-09-23 10:45:36 +0200

    version 2

--- yeezus.py
+++ yeezus.py
@@ -2,18 +2,31 @@ Phone1 = float(input("Hoeveel kost de IPhone? "))
 Phone2 = float(input("Hoeveel kost de Galaxy? "))
 Difference = 0
 
-if Phone1 > Phone2:
-    print("De Iphone is het duurst, de telefoon kost: " + str(Phone1) + " Euro.")
-    print("De Galaxy is het goedkoopst, de telefoon kost: " + str(Phone2) + " Euro.")
-    Difference = Phone1 - Phone2
-    if Difference > 50:
-        print("Koop de Galaxy. De Iphone is " + str(Difference) + " euro duurder.")
-    else:
-        print("Koop de IPhone. De Iphone is maar " + str(Difference) + " euro duurder.")
-if Phone1 < Phone2:
-    print("De Galaxy is het duurst, de telefoon kost: " + str(Phone2) + " Euro.")
-    print("De IPhone is het goedkoopst, de telefoon kost: " + str(Phone1) + " Euro.")
-    print("Koop de Iphone. De Iphone is " + str(Difference) + " euro goedkoper.")
-if Phone1 == Phone2:
-    print("Koop de IPhone. Ze zijn beide even duur.")
 if Phone1 > 900:
+    print("Koop de Galaxy. De IPhone is te duur.")
+    exit()
+if Phone2 > 900:
+    print("Koop de IPhone. De Galaxy is te duur.")
+    exit()
+if Phone1 > 900 and Phone2 > 900:
+    print("Koop geen telefoon. Ze zijn te duur.")
+    exit()
+else:
+    if Phone1 > Phone2:
+        print("De Iphone is het duurst, de telefoon kost: " + str(Phone1) + " Euro.")
+        print("De Galaxy is het goedkoopst, de telefoon kost: " + str(Phone2) + " Euro.")
+        Difference = Phone1 - Phone2
+        if Difference > 50:
+            print("Koop de Galaxy. De Iphone is " + str(Difference) + " euro duurder.")
+            exit()
+        else:
+            print("Koop de IPhone. De Iphone is maar " + str(Difference) + " euro duurder.")
+            exit()
+    if Phone1 < Phone2:
+        print("De Galaxy is het duurst, de telefoon kost: " + str(Phone2) + " Euro.")
+        print("De IPhone is het goedkoopst, de telefoon kost: " + str(Phone1) + " Euro.")
+        print("Koop de Iphone. De Iphone is " + str(Difference) + " euro goedkoper.")
+        exit()
+    if Phone1 == Phone2:
+        print("Koop de IPhone. Ze zijn beide even duur.")
+        exit()
